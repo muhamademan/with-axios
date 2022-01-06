42b614a205acd7968a728e755e30414184d9d096
Muhamad Eman Sulaeman <37392560+muhamademan@users.noreply.github.com>
2022-01-06 11:03:01 +0700

    modified file App,js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './style.css'
+import Header from "./header/header";
 
-function App() {
+const Peserta = () => {
+  const [pesertaLomba, setPesertaLomba] = useState([])
+  const [inputName, setInputName] = useState('')
+  const [curretnId, setCurrentId] = useState(null)
+
+  // FUNGSI BUTTON HANDLE EDIT
+  const handleEdit = (event) => {
+    let idPeserta = event.target.value
+    axios.get(`http://backendexample.sanbercloud.com/api/contestants/${idPeserta}`)
+      .then(res => {
+        let data = res.data
+        setInputName(data.name)
+        setCurrentId(data.id)
+      })
+  }
+
+  // FUNGSI BUTTON HANDLE DELETE
+  const handleDelete = (event) => {
+    let idPeserta = parseInt(event.target.value)
+    axios.delete(`http://backendexample.sanbercloud.com/api/contestants/${idPeserta}`)
+      .then(() => {
+        let newPesertaLomba = pesertaLomba.filter(el => {
+          return el.id !== idPeserta
+        })
+        setPesertaLomba(newPesertaLomba)
+      })
+  }
+
+  // FUNGSI handleChange
+  const handleChange = (event) => {
+    let inputValue = event.target.value
+    setInputName(inputValue)
+  }
+
+  // FUNGSI handleSubmit
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (curretnId === null) {
+      // Untuk create data baru
+      axios.post(`http://backendexample.sanbercloud.com/api/contestants`, { name: inputName })
+        .then(res => {
+          let data = res.data
+          setPesertaLomba([...pesertaLomba, { id: data.id, name: data.name }])
+        })
+    } else {
+      axios.put(`http://backendexample.sanbercloud.com/api/contestants/${curretnId}`, { name: inputName })
+        .then(() => {
+          let singlePeserta = pesertaLomba.find(el => el.id === curretnId)
+          singlePeserta.name = inputName
+          setPesertaLomba([...pesertaLomba])
+        })
+    }
+    setInputName('')
+    setCurrentId(null)
+  }
+
+
+  useEffect(() => {
+    const fetcData = async () => {
+      const result = await axios.get(`http://backendexample.sanbercloud.com/api/contestants`)
+
+      setPesertaLomba(result.data.map(x => {
+        return { id: x.id, name: x.name }
+      }))
+    }
+    fetcData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      {
+        pesertaLomba !== null &&
+        (
+          <div style={{ width: '70%', margin: '0 auto', textAlign: 'center' }}>
+            <table className="peserta-lomba">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Peserta</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  pesertaLomba.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{item.name}</td>
+                        <td>
+                          <button onClick={handleEdit} value={item.id}>Edit</button>
+                          &nbsp;
+                          <button onClick={handleDelete} value={item.id}>Delete</button>
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+            <h1>Form Input Peserta</h1>
+            <form className="inputan" onSubmit={handleSubmit}>
+              <label>Masukan Nama Anda</label>&nbsp;
+              <input type='text' value={inputName} onChange={handleChange}></input>&nbsp;
+              <button>Submit</button>
+            </form>
+          </div>
+        )
+      }
+    </>
+  )
 }
 
-export default App;
+export default Peserta
