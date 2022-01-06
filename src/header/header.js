3efc1e34dea7a63cc478a484706cec5730db6a9c
Muhamad Eman Sulaeman <37392560+muhamademan@users.noreply.github.com>
2022-01-06 11:03:39 +0700

    added folder header and file header.js

---
+++ src/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const header = () => {
+    return (
+        <>
+            <div className="header">
+                <h1>Daftar Peserta Lomba Menggunakan API</h1>
+            </div>
+        </>
+    )
+}
+export default header
